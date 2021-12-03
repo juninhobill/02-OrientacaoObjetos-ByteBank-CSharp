248a2e8f3979d143d0109a75d935552881d2c4bb
juninhobill <billv6@gmail.com>
2021-12-03 08:42:55 -0300

    Finalização Módulo 2 - Intordução a Orientação a Objetos.

---
+++ 07-ByteBank/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_ByteBank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ContaCorrente.TotalDeContasCriadas);
+            ContaCorrente conta = new ContaCorrente(876,3231231);
+            Console.WriteLine(ContaCorrente.TotalDeContasCriadas);
+
+            Console.WriteLine(conta.Agencia);
+            Console.WriteLine(conta.Numero);
+
+            ContaCorrente contaDaGabriela = new ContaCorrente(898, 324234);
+
+            Console.WriteLine(ContaCorrente.TotalDeContasCriadas);
+
+            Console.ReadLine();
+
+        }
+    }
+}
